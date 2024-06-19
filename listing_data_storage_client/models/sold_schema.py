1281ccfaecf1ae709ca1e1bc2437918690ec819c
Jiri Sevcik <jiri.sevcik@heureka.group>
2024-06-19 12:38:35 +0200

    Update inputs

--- listing_data_storage_client/models/sold_schema.py
+++ listing_data_storage_client/models/sold_schema.py
@@ -17,18 +17,22 @@ import pprint
 import re  # noqa: F401
 import json
 
-from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, StrictStr
 from typing import Any, ClassVar, Dict, List, Optional
-from listing_data_storage_client.models.validation_error import ValidationError
 from typing import Optional, Set
 from typing_extensions import Self
 
-class HTTPValidationError(BaseModel):
+class SoldSchema(BaseModel):
     """
-    HTTPValidationError
+    SoldSchema
     """ # noqa: E501
-    detail: Optional[List[ValidationError]] = None
-    __properties: ClassVar[List[str]] = ["detail"]
+    section: StrictStr
+    row: StrictStr
+    place_id: StrictStr
+    price: Optional[StrictStr]
+    sold: datetime
+    __properties: ClassVar[List[str]] = ["section", "row", "place_id", "price", "sold"]
 
     model_config = ConfigDict(
         populate_by_name=True,
@@ -48,7 +52,7 @@ class HTTPValidationError(BaseModel):
 
     @classmethod
     def from_json(cls, json_str: str) -> Optional[Self]:
-        """Create an instance of HTTPValidationError from a JSON string"""
+        """Create an instance of SoldSchema from a JSON string"""
         return cls.from_dict(json.loads(json_str))
 
     def to_dict(self) -> Dict[str, Any]:
@@ -69,18 +73,16 @@ class HTTPValidationError(BaseModel):
             exclude=excluded_fields,
             exclude_none=True,
         )
-        # override the default output from pydantic by calling `to_dict()` of each item in detail (list)
-        _items = []
-        if self.detail:
-            for _item_detail in self.detail:
-                if _item_detail:
-                    _items.append(_item_detail.to_dict())
-            _dict['detail'] = _items
+        # set to None if price (nullable) is None
+        # and model_fields_set contains the field
+        if self.price is None and "price" in self.model_fields_set:
+            _dict['price'] = None
+
         return _dict
 
     @classmethod
     def from_dict(cls, obj: Optional[Dict[str, Any]]) -> Optional[Self]:
-        """Create an instance of HTTPValidationError from a dict"""
+        """Create an instance of SoldSchema from a dict"""
         if obj is None:
             return None
 
@@ -88,7 +90,11 @@ class HTTPValidationError(BaseModel):
             return cls.model_validate(obj)
 
         _obj = cls.model_validate({
-            "detail": [ValidationError.from_dict(_item) for _item in obj["detail"]] if obj.get("detail") is not None else None
+            "section": obj.get("section"),
+            "row": obj.get("row"),
+            "place_id": obj.get("place_id"),
+            "price": obj.get("price"),
+            "sold": obj.get("sold")
         })
         return _obj
 
